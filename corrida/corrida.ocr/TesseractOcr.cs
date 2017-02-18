4ac0771b650209f4eed3f6c95e80de83105a544e
Corina Blajovici <corina.blajovici@iquestgroup.com>
2017-02-19 00:24:24 +0200

    fix merge issues

--- corrida/corrida.ocr/TesseractOcr.cs
+++ corrida/corrida.ocr/TesseractOcr.cs
@@ -22,23 +22,11 @@ namespace corrida.ocr
                         var text = page.GetText();
                         result.Pages.Add(text);
                         result.MeanConfidence = page.GetMeanConfidence();
-<<<<<<< HEAD
-
+  
                         using (var iter = page.GetIterator())
                         {
                             iter.Begin();
-                            do
-                            {
-                                result.Pages.Add(iter.GetText(PageIteratorLevel.Block));
-                            } while (iter.Next(PageIteratorLevel.Block));
-
-=======
-
-                        using (var iter = page.GetIterator())
-                        {
-                            iter.Begin();
-                            while (iter.Next(PageIteratorLevel.Block)) ;
->>>>>>> 8ffb850499fe3712d483faeee530392f7ebaee73
+                            while (iter.Next(PageIteratorLevel.Block)) ; 
                         }
                     }
                 }
