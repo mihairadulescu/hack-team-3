4a8b4dab57e97b286a4c8691d35d6cfd38fbee12
Calin <calinoiu.alexandru@agilefreaks.com>
2017-02-19 06:55:46 +0200

    Add initial dialog

---
+++ assistant/system/boot/google_assistant.rb
@@ -0,0 +1,3 @@
+Assistant::Container.finalize(:google_assistant) do |_container|
+  require 'google_assistant'
+end
--- assistant/web/routes/conversation.rb
+++ assistant/web/routes/conversation.rb
@@ -2,10 +2,43 @@
 
 module Assistant
   class Application
-    route 'conversation' do |r|
-      r.is do
+    route 'v1' do |r|
+      r.on :conversations do
         r.post do
-          { result: :ok }
+          params = JSON.parse(r.body.read)
+
+          assistant_response = GoogleAssistant.respond_to(params, r.response) do |assistant|
+            assistant.intent.main do
+              assistant.ask(
+                prompt: '<speak>Hi there! Say something, please.</speak>',
+                no_input_prompt: [
+                  "<speak>If you said something, I didn't hear you.</speak>",
+                  '<speak>Did you say something?</speak>'
+                ]
+              )
+            end
+
+            assistant.intent.text do
+              if assistant.conversation.state == 'invoices missing'
+                if assistant.arguments[0].text_value.start_with?('yes')
+                  assistant.tell('<speak>You are missing that one and the other one</speak>')
+                else
+                  assistant.tell('<speak>Have a wonderful day!</speak>')
+                end
+              else
+                assistant.conversation.state = 'invoices missing'
+                assistant.ask(
+                  prompt: '<speak>Looks like you have 2 invoices missing. Should I tell witch ones?</speak>',
+                  no_input_prompt: [
+                    "<speak>If you said something, I didn't hear you.</speak>",
+                    '<speak>Did you say something?</speak>'
+                  ]
+                )
+              end
+            end
+          end
+
+          assistant_response
         end
       end
     end
